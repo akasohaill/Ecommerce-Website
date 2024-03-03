40839e951bde6b6d193885ca05a3658f560ebdb6
Md Sohail Ansari <121673553+akasohaill@users.noreply.github.com>
2024-03-03 10:05:53 +0530

    Component 1.0

---
+++ ProductDisplay.jsx
@@ -0,0 +1,59 @@
+import React, { useContext } from 'react'
+import './ProductDisplay.css'
+import star_icon from '../Assets/star_icon.png'
+import star_dull_icon from '../Assets/star_dull_icon.png'
+import { ShopContext } from '../../context/ShopContext'
+
+const ProductDisplay = (props) => {
+    const {product}=props;
+    const {addToCart}=useContext(ShopContext)
+  return (
+    <div className='productdisplay'>
+        <div className="productdisplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+            </div>
+            <div className="productdisplay-img">
+                <img className='productdisplay-main-img' src={product.image} alt="" />
+            </div>
+        </div>
+        <div className="productdisplay-right">
+            <h1>{product.name}</h1>
+            <div className="productdisplay-right-star">
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_dull_icon} alt="" />
+                <p>(1220)</p>
+            </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">${product.old_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                <p>Crafted from premium, breathable fabric, this shirt offers unmatched comfort and versatility. The rich olive hue exudes understated elegance, effortlessly transitioning from casual outings to formal affairs. Tailored to perfection, its sleek silhouette promises a flattering fit for every occasion. </p>
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-sizes">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
+            <div className="productdisplay-right-category"><span>Category:</span><span>Women, T-Shirt , Crop top</span></div>
+            <div className="productdisplay-right-category"><span>Tags:</span><span>Modern , Latest</span></div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default ProductDisplay
